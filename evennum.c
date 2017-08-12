483a20c8ad1dcd37123fd2678c38d70ec8b23bca
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-08-12 16:34:19 +0530

    evennum.c

---
+++ evennum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int num,m =10,n=20;
+    printf("Print Odd Numbers in a given range m to n:\n");
+    for (num = m; num <= n; num++)
+        {
+               if (num % 2 == 0)
+                  printf ("%d ", num);
+         }
+} 
