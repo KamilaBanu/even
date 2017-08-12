da8b0f45df01cf2ee783cc330e30eba19bb19e42
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-08-12 16:16:09 +0530

    Update power.c

--- power.c
+++ power.c
@@ -3,11 +3,8 @@
 int main()
 {
    int i=1, x, n, result=1;
-   printf("Enter x and power n ");
    scanf("%d \n %d",&x, &n);
-
    result= pow (x,n);
-
    printf("%d to the power %d is %d", x, n, result);
    return 0;
 }
