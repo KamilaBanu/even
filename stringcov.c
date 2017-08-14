ed65dbe5b6a0de3dcdb1ee3129749c99d4c43c9d
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-08-14 20:54:06 +0530

    stringcov.c

---
+++ stringcov.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+   int n;
+   char marks[3];
+   printf("Please Enter Marks : ");
+   scanf("%s", marks);
+   num = atoi(marks);
+   printf("Marks : %d", n);
+   return (0);
+}
